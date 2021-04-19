548385e84efe8fe42d61322a0891091a49a04d7d
Jaurel Quentin OLIVE <jaurelquentin.olive@campus-igs-toulouse.fr>
2021-04-19 15:59:21 +0200

    commit

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,10 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'tache',
+        loadChildren: () => import('./tache/tache.module').then(m => m.ExoDevopsTacheModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
